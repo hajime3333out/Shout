3956aaee354060266061841777226c378e43e9d4
EC2 Default User <ec2-user@ip-10-134-214-209.ap-southeast-1.compute.internal>
2015-07-29 05:18:38 +0000

    a

--- config/image.php
+++ config/image.php
@@ -11,11 +11,12 @@ return array(
             4 => '4new.ttf',
             5 => '5new.ttf',
             6 => '6new.ttf',
+            7 => '7new.ttf',
         ),
         'bg_color' => 'none',
         'color' => '#ffffff',
-        'width' => 320,
-        'height' => 320,
+        'width' => 640,
+        'height' => 640,
         'delay' => 50
     )
 );
